009703dca8ea7dace566554395339c03e4c12ab2
John 'Snow' Close <SnowdramaGames@gmail.com>
2023-07-29 12:30:38 -0400

    Added select on open feature

--- Runtime/UIRoute.cs
+++ Runtime/UIRoute.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Snowdrama.UI
 {
@@ -10,6 +11,9 @@ namespace Snowdrama.UI
         [SerializeField] private string routeSegment;
         [SerializeField] private GameObject mainContent;
         [SerializeField] private bool startEnabled = false;
+
+
+        [SerializeField] private Selectable objectToSelectOnOpen;
         private void Start()
         {
             _router.RegisterRoute(routeSegment, this);
@@ -32,6 +36,11 @@ namespace Snowdrama.UI
         public void OpenRoute()
         {
             mainContent.SetActive(true);
+
+            if (objectToSelectOnOpen)
+            {
+                objectToSelectOnOpen.Select();
+            }
         }
 
         public void CloseRoute()
